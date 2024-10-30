57aa0c3544aae48491a31a89b1e28ce3337abffc
Nolberto Luis Sumaran Pimentel <nolberto.sumaran@gmail.com>
2024-10-30 15:40:01 -0500

    pruevaduplicada

---
+++ hola.php
@@ -0,0 +1,44 @@
+<?php
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+?>
+<?php
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+echo "hola\n";
+?>
